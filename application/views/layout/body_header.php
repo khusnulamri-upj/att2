ad8524893c94b6d92993308f55e604fc54f4a29e
Amri <Amri@UPJ-Khusnul>
2013-10-22 15:58:43 +0700

    master

---
+++ application/views/layout/body_header.php
@@ -0,0 +1,54 @@
+  <body>
+    <header class="padded">
+      <div class="container">
+        <div class="row">
+          <div class="one half">
+            <h2 class="logo"><a href="/" target="_parent"><img src="<?= base_url('application/public/image') ?>/upj_logo.png"><b>Attendance</b><sub style="font-size: 14px; margin-left: 5px;">Information System</sub></a></h2>
+          </div>
+          <div class="one half">
+            <p class="small double pad-top no-pad-small-tablet align-right align-left-small-tablet"><a href="#" target="_parent">User Login</a> - Version 2.3.5<br>Created & Maintained by <a href="http://twitter.com/ghepting" target="_blank">Gary Hepting</a></p>
+          </div>
+        </div>
+        <nav role="navigation" class="nav gap-top">
+          <ul role="menubar">
+            <li role="menu">
+              <button>Presensi Karyawan/Dosen</button>
+              <ul>
+                <li><a href="#">Input Keterangan</a></li>
+                <li role="menu">
+                <button title="Laporan Presensi">Laporan Presensi</button>
+                <ul>
+                    <li><a href="./navigation.html" title="Navigation">Per Karyawan Per Bulan</a></li>
+                    <li><a href="./buttons.html" title="Buttons, button groups, button menus">Per Bagian/Prodi Per Tahun</a></li>
+                </ul>
+                </li>
+              </ul>
+            </li>
+            <li role="menu">
+              <button>Documentation</button>
+              <ul>
+                <li><a href="./grid.html" title="Responsive grid system, grid adapters and helpers">Grid</a></li>
+                <li><a href="./helpers.html" title="Layout helpers, spinners and much more">Helpers</a></li>
+                <li><a href="./typography.html" title="Text elements, quotes, code and web fonts">Typography</a></li>
+                <li role="menu">
+                  <button title="Navigation, buttons, boxes, message boxes, tables, tabs, and forms">UI Elements</button>
+                  <ul>
+                    <li><a href="./navigation.html" title="Navigation">Navigation</a></li>
+                    <li><a href="./buttons.html" title="Buttons, button groups, button menus">Buttons</a></li>
+                    <li><a href="./boxes.html" title="Boxes">Boxes</a></li>
+                    <li><a href="./messages.html" title="Message boxes">Message Boxes</a></li>
+                    <li><a href="./tables.html" title="Tables">Tables</a></li>
+                    <li><a href="./tabs.html" title="Tabs">Tabs</a></li>
+                    <li><a href="./forms.html" title="Form elements">Form Elements</a></li>
+                  </ul>
+                </li>
+                <li><a href="./icons.html" title="Icons">Icons</a></li>
+                <li><a href="./responsive-text.html" title="Responsive text and multi-line text block truncation">Responsive Text</a></li>
+                <li><a href="./placeholder-text.html" title="Placeholder text and placeholder fonts for rapid prototyping and wireframes">Placeholder Text</a></li>
+                <li><a href="./animations.html" title="Pure CSS3 Animations">Animations</a></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
---
+++ application/views/layout/head.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html><!--[if lt IE 7]><html class="no-js ie ie6 lt-ie7 lt-ie8 lt-ie9 lt-ie10"><![endif]-->
+<!--[if IE 7]>   <html class="no-js ie ie7 lt-ie8 lt-ie9 lt-ie10"><![endif]-->
+<!--[if IE 8]>   <html class="no-js ie ie8 lt-ie9 lt-ie10"><![endif]-->
+<!--[if IE 9]>   <html class="no-js ie ie9 lt-ie10"><![endif]-->
+<!--[if gt IE 9]><html class="no-js ie ie10"><![endif]-->
+<!--[if !IE]><!-->
+<html class="no-js"><!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <title>Human Resources Information System &hearts; Universitas Pembangunan Jaya</title>
+    <!-- Modernizr -->
+    <script src="<?= base_url('application/public/js/groundwork') ?>/libs/modernizr-2.6.2.min.js"></script>
+    <!-- jQuery-->
+    <script type="text/javascript" src="<?= base_url('application/public/js/groundwork') ?>/libs/jquery-1.10.2.min.js"></script>
+    <!-- framework css --><!--[if gt IE 9]><!-->
+    <link type="text/css" rel="stylesheet" href="<?= base_url('application/public/style/groundwork/css') ?>/groundwork.css"><!--<![endif]--><!--[if lte IE 9]>
+    <link type="text/css" rel="stylesheet" href="<?= base_url('application/public/style/groundwork/css') ?>/groundwork-core.css">
+    <link type="text/css" rel="stylesheet" href="<?= base_url('application/public/style/groundwork/css') ?>/groundwork-type.css">
+    <link type="text/css" rel="stylesheet" href="<?= base_url('application/public/style/groundwork/css') ?>/groundwork-ui.css">
+    <link type="text/css" rel="stylesheet" href="<?= base_url('application/public/style/groundwork/css') ?>/groundwork-anim.css">
+    <link type="text/css" rel="stylesheet" href="<?= base_url('application/public/style/groundwork/css') ?>/groundwork-ie.css"><![endif]-->
+    <style type="text/css">
+      .logo {
+        position: relative;
+        top: -0.5em;
+      }
+      .logo a, .logo a:visited {
+        text-decoration: none;
+        color: #2B2B2D;
+      }
+      .logo img {
+        height: 2em;
+        position: relative;
+        top: 0.55em;
+        margin-right: 0.3em;
+      }
+      
+    </style>
+  </head>
